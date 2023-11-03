d9cfb7fc984983b20f56b8c7fc832e2b9e80ef6f
刘家璇 <1657847221@qq.com>
2023-11-03 15:25:27 +0800

    初始化

---
+++ dao/gorm.go
@@ -0,0 +1,27 @@
+package dao
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func init() {
+	//配置MySQL连接参数
+	username := "root"  //账号
+	password := "root"  //密码
+	host := "127.0.0.1" //数据库地址，可以是Ip或者域名
+	port := 3306        //数据库端口
+	Dbname := "gorm"    //数据库名
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, Dbname)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic("连接数据库失败, error=" + err.Error())
+	}
+	DB = db
+}
